29a93d90c1ec7e0a8655356f17ac33862dadf9b7
Sandrine IBRAIMA-NOMBO <sandrineibraima@gmail.com>
2024-02-20 20:12:28 +0100

    addition of sorting by continent

--- app/components/Countries.js
+++ app/components/Countries.js
@@ -1,41 +1,62 @@
-"use client"
-import React, { useEffect, useState } from 'react';
-import axios from 'axios'
-import styles from '../../assets/styles/components/countries.module.scss'
-import Card from './Card';
+"use client";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import styles from "../../assets/styles/components/countries.module.scss";
+import Card from "./Card";
 
 const Countries = () => {
+  const [data, setData] = useState([]);
+  const [rangeValue, setRangeValue] = useState(36);
+  const radios = ["Africa", "America", "Asia", "Europe", "Oceania"];
+  const [selectedRadio, setSelectedRadio] = useState("");
 
-    const [data, setData] = useState([]);
-    const [rangeValue, setRangeValue] = useState(36)
+  // country data fetch
+  useEffect(() => {
+    try {
+      axios
+        .get("https://restcountries.com/v3.1/all")
+        .then((response) => setData(response.data));
+      console.log(data);
+    } catch (error) {
+      console.log("les données n'ont pas été récupérées", error);
+    }
+  }, []);
 
-    // country data fetch
-    useEffect(()=>{
-        try{            
-            axios.get("https://restcountries.com/v3.1/all")
-            .then((response) =>setData(response.data))
-            console.log(data);
-        }catch(error){
-            console.log("les données n'ont pas été récupérées", error);
-        }
-    }, [])
-
-    return (
-        <div className={styles.countries}>
-            <ul className={styles.radioContainer}>
-               <input type="range" min={1} max={250} defaultValue={rangeValue} onChange={(e) => setRangeValue(e.target.value)}/> 
-            </ul>            
-            <ul>
-                {
-                   data
-                    .slice(0,rangeValue)
-                    .map((country, index) =>(
-                    <Card key={index} country={country}/>
-                   )) 
-                }
-            </ul>
-        </div>
-    );
+  return (
+    <div className={styles.countries}>
+      <ul className={styles.radioContainer}>
+        <input
+          type="range"
+          min={1}
+          max={250}
+          defaultValue={rangeValue}
+          onChange={(e) => setRangeValue(e.target.value)}
+        />
+        {radios.map((continent, index) => (
+          <li key={index}>
+            <input 
+                type="radio" 
+                id={continent}
+                checked={continent === selectedRadio} 
+                onChange={(e)=> 
+                setSelectedRadio(e.target.id)}/>
+            <label htmlFor={continent}>{continent}</label>
+          </li>
+        ))}
+      </ul>
+      {selectedRadio && (
+        <button onClick={()=> setSelectedRadio("")}>Annuler la recherche</button>
+      )}
+      <ul>
+        {data            
+            .filter((country)=> country.continents[0].includes(selectedRadio))
+            .sort((a,b)=> b.population - a.population)
+            .slice(0, rangeValue).map((country, index) => (
+          <Card key={index} country={country} />
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default Countries;
