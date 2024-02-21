98357f9a277008d5c3e609fd4cd2c8b0c736cadd
Sandrine IBRAIMA-NOMBO <sandrineibraima@gmail.com>
2024-02-21 10:01:16 +0100

    filling in the About page

--- app/about/page.js
+++ app/about/page.js
@@ -1,17 +1,39 @@
-import React from 'react';
+import React from "react";
+import styles from '../../assets/styles/components/about.module.scss'
+import Image from 'next/image'
+import Logo from "../components/Logo";
+import Menu from "../components/Menu";
 
 const About = () => {
-    return (
-        <div>
-            <h1>A propos</h1>
-            <br />
-            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magni, aliquid aperiam aut fugiat quidem perspiciatis eligendi placeat nam alias, sequi, non nisi! Aperiam sint natus voluptatum ipsam harum incidunt nihil, consequuntur deserunt veniam praesentium, asperiores quia possimus tenetur aliquam, earum vel voluptates. Eum distinctio, ad excepturi consequuntur nesciunt amet officia iure sed earum? Saepe quaerat, id labore quasi voluptate nihil! Nemo nobis quos ab ipsum tempore officiis a est omnis? Maiores, ipsum. Iure, debitis. Facere tempora repellat suscipit inventore ducimus atque enim possimus corporis quam commodi ipsam, corrupti rem illum doloremque! Labore ratione aut beatae aliquam aliquid, inventore deserunt. Necessitatibus?</p>
+  return (
+    <div className={styles.about}>
+        <Logo/>
+        <Menu />
+        <h1>The countries of the world</h1>
+      <p>
+        There are <b>195 countries</b> in the world today. This total comprises 193
+        countries that are member states of the United Nations and 2 countries
+        that are non-member observer states: the Holy See and the State of
+        Palestine.
+      </p>
 
-            <br />
+      <Image src="/Map.jpg" width={500} height={300} alt="map of the world"/>
 
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus cum maxime quae saepe. Dolore reiciendis aut voluptates magnam, minima aspernatur! Nulla eveniet quasi assumenda saepe, vitae nobis? Obcaecati tempore architecto ea! Unde atque, odio, veniam et nulla quis pariatur animi fuga perspiciatis qui, dolorem sapiente minima optio? Distinctio dolorem dolores ut. Laboriosam exercitationem animi, voluptatem molestias aspernatur beatae provident nihil necessitatibus illum saepe dicta doloribus delectus libero debitis doloremque atque magnam! Doloremque nihil officiis sequi vitae nam beatae sapiente odit iusto sed impedit tempora vel magni odio aspernatur aliquid recusandae modi dolor, nemo enim perspiciatis quis doloribus ipsa maiores? Qui voluptate error molestias fugiat quos recusandae. Dolore voluptatem nisi nostrum a cupiditate doloribus magni consequatur quibusdam temporibus, dicta asperiores architecto quos sed sequi odio perspiciatis distinctio voluptatibus corrupti iusto quis mollitia? Necessitatibus voluptatem, aliquam libero obcaecati porro perspiciatis tempora iusto veritatis numquam molestiae, nostrum quam. Voluptates, dolorem amet dicta sapiente itaque, iusto aperiam eos perferendis nesciunt iure in veritatis aliquid ducimus, odio voluptas sint error natus quibusdam voluptatem dolor qui deleniti sed maiores veniam. Labore sapiente aut velit unde quasi, assumenda impedit facere ipsum voluptatibus facilis eum commodi? Perspiciatis saepe aliquam omnis culpa ipsum impedit, quasi sunt eaque ipsam eos?</p>
-        </div>
-    );
+     
+      <h2>Where are they located ?</h2>
+
+      <p>Of the 195 countries in the world:</p>
+
+      <ul>
+        <li>54 countries are in Africa</li>
+        <li>48 in Asia</li>
+        <li>44 in Europe</li>
+        <li>33 in Latin America and the Caribbean</li>
+        <li>14 in Oceania</li>
+        <li>2 in Northern America</li>
+      </ul>
+    </div>
+  );
 };
 
 export default About;
