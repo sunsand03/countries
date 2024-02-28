b639887c70d8de628ab78feb78a1801617efa648
Sandrine IBRAIMA-NOMBO <sandrineibraima@gmail.com>
2024-02-28 11:38:30 +0100

    add logic to update tip

--- app/components/Tip.js
+++ app/components/Tip.js
@@ -1,7 +1,12 @@
-import React from 'react';
+import React, {useState} from 'react';
 import style from '../../assets/styles/components/tip.module.scss'
+import axios from 'axios'
 
 const Tip = ({tip}) => {
+
+    const [isEditing, setIsEditing]= useState(false);
+    const [editContent, setEditContent]= useState("");
+
 
     const dateFormatter = (date)=>{
         let newDate = new Date(date).toLocaleDateString("fr-FR", {
@@ -15,15 +20,53 @@ const Tip = ({tip}) => {
         return newDate;
     }
 
+    const handleEdit = ()=>{
+        const data ={
+            country : tip.country,
+            content : editContent ? editContent : tip.content,
+            updatedDate: Date.now(),
+    
+        };
+        
+        axios
+        .put('http://localhost:3004/articles/' + tip.id, data)
+        .then(() => {
+          setIsEditing(false);
+        })
+        .catch(error => {
+          // Gestion des erreurs
+          console.error('Error updating article:', error);
+          // Traitez l'erreur selon vos besoins
+        });
+      
+        
+    };
+
     return (
-       <div className={style.tip}>
+       <div className={style.tip} style={{background: isEditing ? "#f3feff" : "white" }}>
         <div className={style.header}>
             <h3>{tip.country}</h3>
             <em>Posté le {dateFormatter(tip.date)}</em>
         </div>
-        <p>{tip.content}</p>
+        {
+            isEditing ? (
+                <textarea 
+                    defaultValue={editContent ? editContent: tip.content}
+                    autoFocus 
+                    onChange={(e)=> setEditContent(e.target.value)}></textarea>
+            ):(
+                <p>{editContent ?  editContent : tip.content}</p>
+            )
+               
+        }
         <div className={style.btnContainer}>
-            <button>Edit</button>
+            {
+                isEditing ? (
+                    <button onClick={()=>handleEdit()}>Valider</button>
+                ) : (
+                    <button onClick={()=>setIsEditing(true)}>Edit</button>
+                )
+            }
             <button>Remove</button>
         </div>
 
